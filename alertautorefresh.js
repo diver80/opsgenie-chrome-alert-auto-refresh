d0ea1a55881e86360571854e433101b8c36964d4
Frank Hess <diver80@users.noreply.github.com>
2020-12-16 10:18:21 +0100

    Update alertautorefresh.js

--- alertautorefresh.js
+++ alertautorefresh.js
@@ -1,4 +1,26 @@
-function autoRefresh() {
-    document.querySelector('div.og-saved-search__list__section__content__item--active').click();
-}
-setInterval(autoRefresh,30000);
+const autoRefreshAlertList = () => {
+    isUpdatePossible = () => {
+        //check we are on alert-list
+        if (!document.getElementById('alert-list')){
+            return false;
+        }
+        //search dialog open
+        if (!(window.getComputedStyle(document.getElementById('assistant-frame')).display === "none")){
+            return false;
+        }
+        //serch bar has focus
+        if (document.querySelector('#editorA.ace_focus')){
+            return false;
+        }
+        return true;
+    } 
+  return ({
+    autoRefresh: () => {
+        if (isUpdatePossible()){
+            document.querySelector('div.og-saved-search__list__section__content__item--active').click();
+        }
+      }
+    });
+  };
+const autoUpdate = autoRefreshAlertList()
+setInterval(autoUpdate.autoRefresh,30000);
